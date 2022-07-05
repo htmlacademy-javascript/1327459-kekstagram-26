a3d58b956926ce80e0366ca5fdf08316c52599b1
Artem Malyutin <81317413+ManInUniverse@users.noreply.github.com>
2022-07-05 22:08:15 +0500

    Изменения от 05.07.2022

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
+const NAMES = [
+  'Артем Малютин',
+  'Арнольд Шварценеггер',
+  'Сильвестр Сталоне',
+  'Джонни Дэпп',
+  'Том Круз',
+  'Пендальф Синий',
+  'Ленс Армстронг',
+  'Сосо Павлиашвили',
+  'Валерий Леонтьев',
+  'Саша Барон Коэн'
+];
+const PHRASES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+const DESCRIPTIONS = [
+  'Любимая фотография.',
+  'Из воспоминаний.',
+  'Фотография на память.',
+  'Я был пьян, когда делал это фото.',
+  'И все-таки, классное получилось фото.',
+  'Ни у кого нет такой фотографии.',
+  'Эту фотку я сделал на Iphone 13 Pro Max.',
+  'Отправь это фото самому близкому тебе человеку.'
+];
+
 //Функция, возвращающая случайное целое положительное число из заданного интервала
 const getRandomInt = function (numberA, numberB) {
   const rangeStart = Math.ceil(Math.min(Math.abs(numberA), Math.abs(numberB)));
@@ -7,6 +38,7 @@ const getRandomInt = function (numberA, numberB) {
 
 //Функция для проверки длины строки
 const checkStringLength = function (currentString, maxLength) {
+  currentString = String(currentString);
   return currentString.length <= maxLength;
 };
 
@@ -18,29 +50,9 @@ const getRandomArrayElement = function (someArray) {
 };
 
 //Функция, которая генерирует массив из комментариев
-const getComments = function (number = getRandomInt(1, 5)) {
-  const NAMES = [
-    'Артем Малютин',
-    'Арнольд Шварценеггер',
-    'Сильвестр Сталоне',
-    'Джонни Дэпп',
-    'Том Круз',
-    'Пендальф Синий',
-    'Ленс Армстронг',
-    'Сосо Павлиашвили',
-    'Валерий Леонтьев',
-    'Саша Барон Коэн'
-  ];
-  const PHRASES = [
-    'Всё отлично!',
-    'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-  ];
+const getComments = function (maxCommentsNumber = getRandomInt(1, 5)) {
   const commentsArray = [];
-  for (let i = 0; i < number; i++) {
+  for (let i = 0; i < maxCommentsNumber; i++) {
     commentsArray[i] = {
       id: null,
       avatar: `img/avatar-${getRandomInt(1, 6)}.svg`,
@@ -52,21 +64,10 @@ const getComments = function (number = getRandomInt(1, 5)) {
 };
 
 //Функция, которая генерирует массив из фотографий
-const getPhotoExpositions = function (number = 25) {
-  const DESCRIPTIONS = [
-    'Любимая фотография.',
-    'Из воспоминаний.',
-    'Фотография на память.',
-    'Я был пьян, когда делал это фото.',
-    'И все-таки, классное получилось фото.',
-    'Ни у кого нет такой фотографии.',
-    'Эту фотку я сделал на Iphone 13 Pro Max.',
-    'Отправь это фото самому близкому тебе человеку.'
-  ];
+const getPhotoExpositions = function (maxPhotosNumber = 25) {
   const photoExpositionsArray = [];//Массив для хранения объектов фотографий
   let idForCommentsCurrent = 1;//Переменная-счетчик, необходимая, чтобы id у комментариев не повторялся
-
-  for (let i = 0; i < number; i++) {
+  for (let i = 0; i < maxPhotosNumber; i++) {
     const commentsArrayResult = getComments();
     photoExpositionsArray[i] = {
       id: i + 1,
@@ -79,7 +80,6 @@ const getPhotoExpositions = function (number = 25) {
       commentsArrayResult[j].id = idForCommentsCurrent;
       idForCommentsCurrent++;
     }
-
   }
   return photoExpositionsArray;
 };
