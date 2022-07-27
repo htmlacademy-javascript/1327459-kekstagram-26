61e2f60eb2554815548bfcea034aef131505db8f
Artem Malyutin <malyutinartem92@gmail.com>
2022-07-27 22:21:57 +0500

    Работа по замечаниям 27.07.2022

--- js/util.js
+++ js/util.js
@@ -1,27 +1,5 @@
 const ALERT_SHOW_TIME = 10000;
 
-const getRandomInt = (numberA, numberB) => {
-  const rangeStart = Math.ceil(Math.min(Math.abs(numberA), Math.abs(numberB)));
-  const rangeEnd = Math.floor(Math.max(Math.abs(numberA), Math.abs(numberB)));
-  return Math.floor(Math.random() * (rangeEnd - rangeStart + 1)) + rangeStart;
-};
-
-const getRandomUniqIntNumbersFromRange = (min, max, countNumbers) => {
-  const rangeLength = (Math.floor(Math.max(Math.abs(min), Math.abs(max))) - Math.ceil(Math.min(Math.abs(min), Math.abs(max)))) + 1;
-  const numbers = [];
-  if (countNumbers <= rangeLength) {
-    for (let i = 0; i < countNumbers; i++) {
-      let currentNumber = getRandomInt(min, max);
-      while (numbers.includes(currentNumber)) {
-        currentNumber = getRandomInt(min, max);
-      }
-      numbers[i] = currentNumber;
-    }
-    return numbers;
-  }
-  return false;
-};
-
 const checkStringLength = (currentString, maxLength) => {
   currentString = String(currentString);
   return currentString.length <= maxLength;
@@ -80,4 +58,4 @@ const showMessage = (typeOfMessage) => {
   }
 };
 
-export {getRandomUniqIntNumbersFromRange, checkStringLength, showAlert, showMessage};
+export {checkStringLength, showAlert, showMessage};
